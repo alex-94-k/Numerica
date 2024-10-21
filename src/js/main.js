7d2bdf16cdf48601f0b4cd3e29b9e49bff3ec4b0
alex-94-k <al941994.94@gmail.com>
2024-10-21 21:13:54 +0200

    Modal window is done

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,23 @@ const menu = document.querySelector('.navbar__list'),
 burger = document.querySelector('.navbar__burger');
 
 burger.addEventListener('click', function(){
-burger.classList.toggle('navbar__burger-active');
-menu.classList.toggle('navbar__list-active');
+    burger.classList.toggle('navbar__burger-active');
+    menu.classList.toggle('navbar__list-active');
+});
+
+/* Modal */
+
+const btn = document.querySelector('.showcase__btn');
+const overlay = document.querySelector('.overlay');
+const modal = document.querySelector('.modal');
+const closeBtn = document.querySelector('.close')
+
+btn.addEventListener('click', function(){
+    overlay.classList.add('overlay__active');
+    modal.classList.add('modal__active');
+});
+
+closeBtn.addEventListener('click', function(){
+    overlay.classList.remove('overlay__active');
+    modal.classList.remove('modal__active');
 });
